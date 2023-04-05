3ba4dfa91c1b1fb63ef331e405567dd9aae957f7
imen42 <yahyaimen971@gmail.com>
2023-04-05 10:41:03 +0100

    Your commit message here

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require('express')
 const path = require('path')
-
 const app = express();
-app.get ('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'));
-});
+// app.get ('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'public','index.html'));
+// });
 app.use((req, res, next) => {
     const date = new Date();
     const day = date.getDay();
     const hour = date.getHours();
-    if (day > 0 && day < 6 && hour > 9 && hour < 17) {
+    if (day > 0 && day < 6 && hour >= 9 && hour < 17) {
       next();
     } else {
       res.send("<h1>Not available at this time!</h1>");
     }
   });
   
-  app.use(express.static("pages"));
+  app.use(express.static(path.join(__dirname, "public")));
   
-  var PORT = process.env.PORT || 5000;
+  var PORT = process.env.PORT || 3000;
   app.listen(PORT, (err) =>
     err ? console.log(err) : console.log(`Server is running on port ${PORT}`)
   );
