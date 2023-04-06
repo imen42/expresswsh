3c4a9aa17da99a0d9ef89d5672ebc9c4e9443f50
imen42 <yahyaimen971@gmail.com>
2023-04-06 01:33:46 +0100

    Your commit message here

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const app = express();
 // app.get ('/', (req,res)=>{
 //     res.sendFile(path.join(__dirname,'public','index.html'));
 // });
-app.use((req, res, next) => {
-    const date = new Date();
-    const day = date.getDay();
-    const hour = date.getHours();
-    if (day > 0 && day < 6 && hour >= 9 && hour < 17) {
-      next();
-    } else {
-      res.send("<h1>Not available at this time!</h1>");
-    }
-  });
+// app.use((req, res, next) => {
+//     const date = new Date();
+//     const day = date.getDay();
+//     const hour = date.getHours();
+//     if (day > 0 && day < 6 && hour >= 9 && hour < 17) {
+//       next();
+//     } else {
+//       res.send("<h1>Not available at this time!</h1>");
+//     }
+//   });
   
   app.use(express.static(path.join(__dirname, "public")));
   
